8a51b1fbe4d9a1161579d169ae3373d11243afb5
Elie Marot <elie.marot@gmail.com>
2015-09-11 15:01:50 +0200

    nouvel agencement du classement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
 
 
   def index
-    @users = User.all
+
+    @users = User.all.flatten.sort! { |a,b| a.total_score <=> b.total_score }.reverse!
+
+    # users => le classement des utilisateurs
     @score = session[:score]
   end
 
